e1e4ea5c3ebdbec2b8c3bc62096da6e77d5f1c09
Myy <502250351@qq.com>
2015-11-04 11:29:13 +0800

    Hy

--- app/src/main/java/cn/project/aoyolo/cooperation_v1/API/Api.java
+++ app/src/main/java/cn/project/aoyolo/cooperation_v1/API/Api.java
@@ -13,8 +13,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-
 
 /**
  * Created by Hy on 2015/11/2.
@@ -24,15 +22,15 @@ public class Api
     public static KJHttp.Builder httpBuilder = new KJHttp.Builder();
     //后台地址
     private final static String RURL="http://andrewlu.cn/SupplyAndDemondPlatform";
-
-
+    private final static String HYDURL="http://192.168.1.114:8080/web";//HY宿舍测试地址
+    private final static String HYGURL="http://192.168.16.2:8080/web";//HY公司地址
     //测试地址
 
 
 
     //现在使用地址
-    public final static String NURL=RURL;
-        //繁琐，失败
+    public final static String NURL=HYGURL;
+
 //    public static String readContentFromPost(URL url,String content) throws IOException{
 //        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 //        //post请求要放在http正文处
@@ -46,6 +44,7 @@ public class Api
 //        // 意思是正文是urlencoded编码过的form参数，下面我们可以看到我们对正文内容使用URLEncoder.encode
 //        // 进行编码
 //        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+//        connection.connect();
 //        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
 //        out.writeBytes(content);
 //        out.flush();
@@ -61,18 +60,14 @@ public class Api
 //    }
 //    public static String uploadFromPost(URL url,File file) throws  IOException{
 //        HttpURLConnection connection =(HttpURLConnection) url.openConnection();
-//        connection.setDoOutput(true);
 //        connection.setDoInput(true);
-//        connection.setRequestMethod("POST");
+//        connection.setDoInput(true);
 //        //POST请求不能用环缓存
 //        connection.setUseCaches(false);
 //        connection.setInstanceFollowRedirects(true);
-//        connection.setRequestProperty("Connection", "Keep-Alive");
-//        connection.setRequestProperty("Charset", "UTF-8");
-//        connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
 //        // 配置本次连接的Content-type，配置为multipart/form-data的
-//        connection.setRequestProperty("Content-type", "multipart/form-data;boundary=*****");
-//        //connection.connect();
+//        connection.setRequestProperty("Content-Type", "multipart/form-data");
+//        connection.connect();
 //        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
 //        FileInputStream in = new FileInputStream(file);
 //        ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -82,8 +77,6 @@ public class Api
 //            output.write(b, 0, n);
 //        }
 //        in.close();
-//        out.writeBytes("pics=");
-//        out.writeBytes("Content-type: " +"application/octet-stream");
 //        out.write(output.toByteArray());
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 //        String line ="";
