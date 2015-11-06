6005b82bbdd5df65baa0155207fd82290fd6b067
Fanyua <Fanyubin829475@outlook.com>
2015-11-06 09:48:05 +0800

    上拉下拉

---
+++ app/src/main/java/cn/project/aoyolo/cooperation_v1/widget/Pullable.java
@@ -0,0 +1,21 @@
+package cn.project.aoyolo.cooperation_v1.widget;
+
+/**
+ * Created by yubin on 2015/11/6.
+ */
+public interface Pullable
+{
+    /**
+     * 判断是否可以下拉，如果不需要下拉功能可以直接return false
+     *
+     * @return true如果可以下拉否则返回false
+     */
+    boolean canPullDown();
+
+    /**
+     * 判断是否可以上拉，如果不需要上拉功能可以直接return false
+     *
+     * @return true如果可以上拉否则返回false
+     */
+    boolean canPullUp();
+}
